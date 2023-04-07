d40b5f7a036050a5c096145bb77c61fe89fa7edb
Wesam Nabeel <wesamezio@gmail.com>
2023-04-07 15:32:00 +0300

    improve code and add more sensors

--- Car_Parking_System_LCD.ino
+++ Car_Parking_System_LCD.ino
@@ -1,9 +1,16 @@
 #include <Servo.h> //includes the servo library
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+const int LCD_COLUMNS = 20;
+const int LCD_ROWS = 4;
+
+LiquidCrystal_I2C lcd(0x27,LCD_COLUMNS, LCD_ROWS);
 
 Servo myservo;
+
+
+#define servomotor 3
 
 #define ir_enter 2
 #define ir_back  4
@@ -14,107 +21,91 @@ Servo myservo;
 #define ir_car4 8
 #define ir_car5 9
 #define ir_car6 10
+#define ir_car7 11
+#define ir_car8 12
 
-int S1=0, S2=0, S3=0, S4=0, S5=0, S6=0;
-int flag1=0, flag2=0; 
-int slot = 6;  
-
+int flag1=0, flag2=0;
+int available_slots = 8;
 void setup(){
-Serial.begin(9600);
-
-pinMode(ir_car1, INPUT);
-pinMode(ir_car2, INPUT);
-pinMode(ir_car3, INPUT);
-pinMode(ir_car4, INPUT);
-pinMode(ir_car5, INPUT);
-pinMode(ir_car6, INPUT);
-
-pinMode(ir_enter, INPUT);
-pinMode(ir_back, INPUT);
   
-myservo.attach(3);
-myservo.write(90);
+  Serial.begin(9600);
+  pinMode(ir_car1, INPUT);
+  pinMode(ir_car2, INPUT);
+  pinMode(ir_car3, INPUT);
+  pinMode(ir_car4, INPUT);
+  pinMode(ir_car5, INPUT);
+  pinMode(ir_car6, INPUT);
+  
+  pinMode(ir_enter, INPUT);
+  pinMode(ir_back, INPUT);
 
-lcd.begin(20, 4);  
-lcd.setCursor (0,1);
-lcd.print("    Car  parking  ");
-lcd.setCursor (0,2);
-lcd.print("       System     ");
-delay (2000);
-lcd.clear();   
+  myservo.attach(servomotor);
+  myservo.write(90);  
 
-Read_Sensor();
+  lcd.init();  
+  lcd.backlight();
 
-int total = S1+S2+S3+S4+S5+S6;
-slot = slot-total; 
+  lcd.setCursor (0,1);
+  lcd.print("    Car  parking  ");
+  lcd.setCursor (0,2);
+  lcd.print("       System     ");
+  delay (2000);
+  
+  lcd.clear(); 
+
 }
 
 void loop(){
-
-Read_Sensor();
-
-lcd.setCursor (0,0);
-lcd.print("   Have Slot: "); 
-lcd.print(slot);
-lcd.print("    ");  
-
-lcd.setCursor (0,1);
-if(S1==1){lcd.print("S1:Fill ");}
-     else{lcd.print("S1:Empty");}
-
-lcd.setCursor (10,1);
-if(S2==1){lcd.print("S2:Fill ");}
-     else{lcd.print("S2:Empty");}
-
-lcd.setCursor (0,2);
-if(S3==1){lcd.print("S3:Fill ");}
-     else{lcd.print("S3:Empty");}
-
-lcd.setCursor (10,2);
-if(S4==1){lcd.print("S4:Fill ");}
-     else{lcd.print("S4:Empty");}
-
- lcd.setCursor (0,3);
-if(S5==1){lcd.print("S5:Fill ");}
-     else{lcd.print("S5:Empty");}
-
-lcd.setCursor (10,3);
-if(S6==1){lcd.print("S6:Fill ");}
-     else{lcd.print("S6:Empty");}    
-
-if(digitalRead (ir_enter) == 0 && flag1==0){
-if(slot>0){flag1=1;
-if(flag2==0){myservo.write(180); slot = slot-1;}
-}else{
-lcd.setCursor (0,0);
-lcd.print(" Sorry Parking Full ");  
-delay(1500);
-}   
-}
-
-if(digitalRead (ir_back) == 0 && flag2==0){flag2=1;
-if(flag1==0){myservo.write(180); slot = slot+1;}
-}
-
-if(flag1==1 && flag2==1){
-delay (1000);
-myservo.write(90);
-flag1=0, flag2=0;
-}
-
-delay(1);
-}
-
-void Read_Sensor(){
-S1=0, S2=0, S3=0, S4=0, S5=0, S6=0;
-
-if(digitalRead(ir_car1) == 0){S1=1;}
-if(digitalRead(ir_car2) == 0){S2=1;}
-if(digitalRead(ir_car3) == 0){S3=1;}
-if(digitalRead(ir_car4) == 0){S4=1;}
-if(digitalRead(ir_car5) == 0){S5=1;}
-if(digitalRead(ir_car6) == 0){S6=1;}  
+  
+  print_on_lcd(ir_car1,0,1,1);
+  print_on_lcd(ir_car2,10,1,2);
+  print_on_lcd(ir_car3,0,2,3);
+  print_on_lcd(ir_car4,10,2,4);
+  print_on_lcd(ir_car5,0,3,5);
+  print_on_lcd(ir_car6,10,3,6);
+  print_on_lcd(ir_car7,0,4,7);
+  print_on_lcd(ir_car8,10,4,8);
+  
+  if(digitalRead (ir_enter) == 0 && flag1==0){
+    if(available_slots>0){
+      flag1=1;
+      if(flag2==0){
+        myservo.write(180); 
+        available_slots = available_slots - 1;
+        }
+  } else{
+    lcd.setCursor (0,0);
+    lcd.print(" Sorry Parking Full ");  
+    delay(1500);
+    }   
+  }
+  
+  if(digitalRead (ir_back) == 0 && flag2==0){
+    flag2=1;
+    if(flag1==0){myservo.write(180); available_slots = available_slots +1;}
+  }
+  
+  if(flag1==1 && flag2==1){
+    delay (1000);
+    myservo.write(90);
+    flag1=0, flag2=0;
+  }
+  
+  delay(1);
 }
 
 
 
+void print_on_lcd(int car,int location_x, int location_y,int slot_number) {
+  lcd.setCursor (location_x,location_y);
+  lcd.print("s");
+  lcd.print(slot_number);
+
+  bool isFilled = false;
+  
+  if(digitalRead(car) == 0){
+      lcd.print(": Full");
+    } else {
+      lcd.print(": Empty");
+    }  
+}
